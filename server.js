69b665c1cf8a19d174d55629fa4cf39a466c21f1
Pawelsmolarski95 <109138583+Pawelsmolarski95@users.noreply.github.com>
2023-01-16 16:32:33 +0100

    fix problem

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatsRoutes);
-app.use(express.static(path.join(__dirname, '/client/build')));
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
