a561c855d4243c78c8efa51493587a97f9e76af7
Pawelsmolarski95 <109138583+Pawelsmolarski95@users.noreply.github.com>
2023-01-16 16:20:04 +0100

    ready to replit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
@@ -12,10 +12,15 @@ app.use(cors());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatsRoutes);
+app.use(express.static(path.join(__dirname, '/client/build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+ 
 app.use((req, res) => {
     res.status(404).json({ message: '404 not found...'})
 });
-app.listen(8000, () => {
-    console.log('Server is running');
-});
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  });
