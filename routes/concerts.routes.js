aa3291774d198359385ed46c6803f8ca82587d26
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2023-02-05 08:33:59 +0100

    add concerts endpoints

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -12,5 +12,15 @@ router.route('/concerts').post(ConcertControllers.postCon);
 router.route('/concerts/:id').delete(ConcertControllers.deleteCon);
 
 router.route('/concerts/:id').put(ConcertControllers.editCon);
+
+router.route('/concerts/performer/:performer').get(ConcertControllers.getPerformer);
+
+router.route('/concerts/genre/:genre').get(ConcertControllers.getGenre);
+
+router.route('/concerts/price/:price_min/:price_max').get(ConcertControllers.getMinMaxPrice);
+
+router.route('/concerts/day/:day').get(ConcertControllers.getConcertDay);
+
+
  
 module.exports = router;
